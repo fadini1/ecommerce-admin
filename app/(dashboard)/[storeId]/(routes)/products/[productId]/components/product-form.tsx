c3ee298169e02fb57942c90328c397f6974eb215
Fadini <fodriniagustin@gmail.com>
2023-11-19 10:50:38 -0300

    test

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -59,12 +59,12 @@ const formSchema = z.object({
   categoryId: z.string().min(1),
   sizeId: z.string().min(1),
   colorId: z.string().min(1),
-
+  
   isFeatured: z.boolean().default(false).optional(),
   isArchived: z.boolean().default(false).optional(),
-
+  
+  quantity: z.coerce.number().int().min(1),
   price: z.coerce.number().min(1),
-  quantity: z.coerce.number().min(1),
 
   images: z.object({ url: z.string() }).array()
 });
